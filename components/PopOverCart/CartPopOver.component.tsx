ed0233d303c8c6c9b2e4f174800110db81c1f211
unknown <shasheen@i-threesixty.com>
2022-07-21 10:02:32 +0530

    Refactor CartPopOver and ProductCard

--- components/PopOverCart/CartPopOver.component.tsx
+++ components/PopOverCart/CartPopOver.component.tsx
@@ -124,14 +124,14 @@ const CartPopOver = () => {
           <Transition
             show={openPopover}
             as={Fragment}
-            enter="transition ease-out duration-200"
+            enter="transition ease-out duration-100"
             enterFrom="opacity-0 translate-y-1"
             enterTo="opacity-100 translate-y-0"
             leave="transition ease-in duration-150"
             leaveFrom="opacity-100 translate-y-0"
             leaveTo="opacity-0 translate-y-1"
           >
-            <Popover.Panel static className={style.popOverPanel}>
+            <Popover.Panel className={style.popOverPanel}>
               <div className={style.popOverPanelHeader}>
                 <h2>Cart</h2>
                 <Popover.Button onClick={closePopOver}>
